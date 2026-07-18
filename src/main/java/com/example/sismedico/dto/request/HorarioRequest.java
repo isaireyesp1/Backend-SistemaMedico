2d56b038f2b9e981ab2d67345814da51183a2c79
Isai Reyes <anonymus1201a@gmail.com>
2026-07-17 18:25:09 -0600

    Creacion del DTO de Horario

--- src/main/java/com/example/sismedico/dto/request/HorarioRequest.java
+++ src/main/java/com/example/sismedico/dto/request/HorarioRequest.java
@@ -1,5 +1,34 @@
 package com.example.sismedico.dto.request;
 
+import com.example.sismedico.enums.DiaSemana;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class HorarioRequest {
-    
+
+    @NotNull(message = "El médico es obligatorio")
+    private Long medicoId;
+
+    @NotNull(message = "El día de la semana es obligatorio")
+    @Enumerated(EnumType.STRING)
+    private DiaSemana diaSemana;
+
+    @NotNull(message = "La hora de inicio es obligatoria")
+    private LocalTime horaInicio;
+
+    @NotNull(message = "La hora de fin es obligatoria")
+    private LocalTime horaFin;
+
+    @Builder.Default
+    private Boolean disponible = true;
+
 }
